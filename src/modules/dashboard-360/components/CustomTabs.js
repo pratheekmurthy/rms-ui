787df114d3f109e86ace7175105523d4e3c35d62
jatinchhabra-1 <jatinc35@gmail.com>
2020-12-10 10:26:12 +0530

    Custom tab - Minor Bug Fix

--- src/modules/dashboard-360/components/CustomTabs.js
+++ src/modules/dashboard-360/components/CustomTabs.js
@@ -6,55 +6,61 @@ import { makeStyles } from '@material-ui/styles';
 import PropTypes from 'prop-types';
 
 function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`
-    };
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
 }
 
 const useStyles = makeStyles(() => ({
-    root: {
-        maxWidth: 50, // a number of your choice
-        width: 50, // a number of your choice
-    },
-    textTransformNone: {
-        textTransform: 'none'
-    }
+  root: {
+    maxWidth: 50, // a number of your choice
+    width: 50 // a number of your choice
+  },
+  textTransformNone: {
+    textTransform: 'none'
+  }
 }));
 
 export default function CustomTabs({
-    tabNames, setCurrent, children, ...props
+  tabNames,
+  setCurrent,
+  children,
+  ...props
 }) {
-    const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-    const styles = useStyles();
+  const styles = useStyles();
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-        setCurrent(newValue);
-    };
-
-    function getTabs() {
-        return tabNames.map((tab, index) => (
-            <Tab label={tab} key={index} {...a11yProps(0)} className={`${styles.root} ${styles.textTransformNone} `} />
-        ));
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    if (setCurrent) {
+      setCurrent(newValue);
     }
+  };
 
-    return (
-        <Paper square>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                {...props}
-            >
-                {getTabs()}
-            </Tabs>
-        </Paper>
-    );
+  function getTabs() {
+    return tabNames.map((tab, index) => (
+      <Tab
+        label={tab}
+        key={`tab-${index}`}
+        {...a11yProps(0)}
+        className={`${styles.root} ${styles.textTransformNone} `}
+      />
+    ));
+  }
+
+  return (
+    <Paper square>
+      <Tabs value={value} onChange={handleChange} {...props}>
+        {getTabs()}
+      </Tabs>
+    </Paper>
+  );
 }
 
 CustomTabs.propTypes = {
-    tabNames: PropTypes.array,
-    setCurrent: PropTypes.func,
-    children: PropTypes.node,
+  tabNames: PropTypes.array,
+  setCurrent: PropTypes.func,
+  children: PropTypes.node
 };
